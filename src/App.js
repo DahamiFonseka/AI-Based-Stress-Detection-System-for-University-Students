f9f9dd7d3207fdb2c9ba134c32eab86bb3035478
Dahami Fonseka <166377876+DahamiFonseka@users.noreply.github.com>
2024-05-19 15:36:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import LoginRegister from './Components/LoginRegister/LoginRegister';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      {/* <LoginRegister /> */}
+    </>
+  );
+}
+
+export default App;
