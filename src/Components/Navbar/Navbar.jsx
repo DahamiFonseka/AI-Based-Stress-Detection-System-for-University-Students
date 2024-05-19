4e25f741d19e09b15dc229745c841a01b6e7d9d6
Dahami Fonseka <166377876+DahamiFonseka@users.noreply.github.com>
2024-05-19 20:20:06 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,45 +1,18 @@
 import React from 'react';
 import './Navbar.css';
-import { AiFillCloseCircle } from "react-icons/ai";
-import { TbGridDots } from "react-icons/tb";
-
 
 const Navbar = () => {
   return (
-    <section classname = 'navbarSection'>
-        <header classname = 'header-flex'>
-            <div classname = 'logoDiv'>
-                {/* <a href = "#" classname = "logo flex"> */}
-                    <h1>STUDENT STRESS DETECTION</h1>
-                {/* </a> */}
-            </div>
+    <header className="header">
+        <a href="/" className = "logo">STRESS DETECTION</a>
 
-            <div className="navbar">
-                <ul className="navbarlists-flex">
-                    <li className="navitem">
-                        <a href="#" className="navlink">Home</a>
-                    </li>
-                    <li className="navitem">
-                        <a href="#" className="navlink">Nav1</a>
-                    </li>
-                    <li className="navitem">
-                        <a href="#" className="navlink">Nav2</a>
-                    </li>
-                    <li className="navitem">
-                        <a href="#" className="navlink">Admin Login</a>
-                    </li>
-                </ul>
-
-                <div className="closenavbar">
-                    <AiFillCloseCircle className="icon"/>
-                </div>
-            </div>
-
-            <div className="toggleNavbar" >
-                <TbGridDots className="icon"/>
-            </div>
-        </header>
-    </section>
+        <nav className="navbar">
+            <a href="/">Dashboard</a>
+            <a href="/">Dashboard</a>
+            <a href="/">Dashboard</a>
+            <a href="/">Dashboard</a>
+        </nav>
+    </header>
   );
 };
 
